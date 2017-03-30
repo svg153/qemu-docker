b972e659b2e88a3cbecb4470dd987f4cf4aaa88e
Sergio Valverde <svg153@users.noreply.github.com>
2017-03-30 12:39:12 +0200

    fix argument --privileged

--- kvm-mknod.sh
+++ kvm-mknod.sh
@@ -10,11 +10,11 @@ lsmod | grep '\<kvm\>' > /dev/null || {
 
 mknod /dev/kvm c 10 $(grep '\<kvm\>' /proc/misc | cut -f 1 -d' ') || {
   echo >&2 "Unable to make /dev/kvm node; software emulation will be used"
-  echo >&2 "(This can happen if the container is run without -privileged)"
+  echo >&2 "(This can happen if the container is run without --privileged)"
   exit 1
 }
 
 dd if=/dev/kvm count=0 2>/dev/null || {
   echo >&2 "Unable to open /dev/kvm; qemu will use software emulation"
-  echo >&2 "(This can happen if the container is run without -privileged)"
+  echo >&2 "(This can happen if the container is run without --privileged)"
 }
